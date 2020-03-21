c2e8ba569eefacb7c6af93f8d5e6cce1c3851e7d
nikitasadok <44681859+nikitasadok@users.noreply.github.com>
2020-03-21 22:22:20 +0200

    Update data.go
    Changed json parameter names

--- handlers/data.go
+++ handlers/data.go
@@ -2,11 +2,11 @@ package handlers
 
 type Employee struct {
 	EmpID         string `yaml:"emp_id" json:"emp_id"`
-	FirstName     string `yaml:"first_name" json:"firstname"`
-	SecondName    string `yaml:"last_name" json:"lastname"`
+	FirstName     string `yaml:"first_name" json:"first_name"`
+	SecondName    string `yaml:"last_name" json:"last_name"`
 	DefaultSalary string `yaml:"default_salary" json:"default_salary"`
-	Experience    string `yaml:"experience" json:"experiance"`
-	Type          string `yaml:"types" json:"position"`
+	Experience    string `yaml:"experience" json:"experience"`
+	Type          string `yaml:"types" json:"types"`
 	Salary        string `json:"salary"`
 }
 
