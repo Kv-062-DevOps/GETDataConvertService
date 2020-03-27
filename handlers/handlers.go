8351e5dc392c0bc15af3be041abaeab6d7635ce1
nikitasadok <44681859+nikitasadok@users.noreply.github.com>
2020-03-27 15:08:34 +0200

    Update handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,10 +7,12 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"fmt"
 	"strconv"
 )
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(os.Getenv("ENDPOINT"))
 	req, err := http.NewRequest("GET", os.Getenv("ENDPOINT"),r.Body)
 	if err != nil {
 		http.Error(w, "Troubles with forming request to DB service", 500)
